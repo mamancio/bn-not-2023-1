bdc1aad369e9f9933a5bbef180fb9c9292c8e5a3
Matheus Mancio <mancio105@gmail.com>
2023-06-13 19:08:04 +0000

    front e back

---
+++ src/routes/index.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import { Login } from '../pages/login'
+import { Home } from '../pages/home'
+import { Navbar } from '../components/Navbar';
+import { Cadastro } from '../pages/cadastro';
+
+
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/login' element={<Login />} />
+                <Route exact path='/servicos' element={<Home />} />
+                <Route exact path='/cadastro' element={<Cadastro />} />
+
+            </Routes>
+        </Router>
+    );
+} 
+
+export default AppRoutes;
