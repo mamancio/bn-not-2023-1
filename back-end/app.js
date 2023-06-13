20b2a9ff05e159d621b20f555b1039cca094afd7
Matheus Mancio <mancio105@gmail.com>
2023-06-13 19:10:12 +0000

    rota back e atlas

--- back-end/app.js
+++ back-end/app.js
@@ -1,48 +1,79 @@
-// Carregar as variaveis de ambiente a partir do arquivo .env
-
 require('dotenv').config()
 
-
-
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-//Conectar ao banco de dados
-require('./config/database')()
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const express = require('express')
+const app = express()
 
 
 
-/*************************************
- * ROTAS
-**************************************/
+const mongoose = require('mongoose')
 
-const customersRouter = require('./routes/customers')
-app.use('/customers', customersRouter)
+const User = require('./models/User')
 
-const salesRouter = require('./routes/sales')
-app.use('/sales', salesRouter)
+const path = require('path')
 
-const suppliersRouter = require('./routes/suppliers')
-app.use('/suppliers', suppliersRouter)
+const templatePath = path.join(__dirname, '../templates')
 
-const productsRouter = require('./routes/products')
-app.use('/products', productsRouter)
 
+const indexRouter = require('./routes/index');
+
+
+mongoose.connect('mongodb+srv://dsmg04pi320231:FATEC1234@cluster0.notixwg.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log(`Conectado ao MongoDB`)
+    
+    app.listen(5000, () => {
+        
+        console.log(`Rodando na porta 5000`)
+    })
+}).catch((error) => { 
+    console.log(error)
+})
+
+
+app.use(express.json())
+app.set('view engine', 'hbs')
+app.set("views", templatePath)
+app.use(express.urlencoded({ extended: false }))
+
+
+//Registrar User
+app.post('/auth/register', async (req, res) =>{
+    const {name, email, password, confirmarpassword } = req.body
+
+    
+    if(name) {
+    return res.status(422).json({ msg: "Campo nome é obrigatório"})
+    }
+})
+
+
+//login user
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ email: req.body.email })
+
+        if (check.senha === req.body.senha) {
+            res.render('home')
+        } else {
+            res.send('Senha Incorreta')
+        }
+    } catch (error) {
+        res.send('Login inválido')
+    }
+})
+
+
+const usuarioRouter = require('./routes/users')
+app.use('/user', usuarioRouter)
+
+//Rotas
+
+const patientsRouter = require('./routes/patients')
+app.use('/patients', patientsRouter)
+
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
+
+const dentistsRouter = require('./routes/dentists')
+app.use('/dentists', dentistsRouter)
 
 module.exports = app;
